70b6945e15d1239c77ae5460d7395f4d26d53ccf
angelbeatsow <117999333+angelbeatsow@users.noreply.github.com>
2023-03-08 02:26:42 +0900

    Create sceneclass.js

---
+++ sceneclass.js
@@ -0,0 +1,32 @@
+class Scene{
+  constructor(){
+    this.objs  = [];
+    this.flags = [];
+    this.nowflag ;
+    this.map;
+    this.nowpoint  = []; //touchtype,color,xzahyou,yzahyou
+    this.lastpoint = []; //touchtype,color,xzahyou,yzahyou
+    this.conectlines = []; //タイルを結ぶ線を入れておく。タイルを消す度空にする。
+    this.elementstate = new Elementstate;
+    this.timer = []; //タイマーのLineをいれておく。widthにアクセスする。
+    this.shuffleMessage ;
+    this.banmenussura =[];
+  }
+  
+  add(obj,isconectline = false){
+    if(isconectline == false)this.objs.push(obj);
+    else this.conectlines.push(obj);
+  }
+  
+  update(canvas){
+    this.onenterframe();
+  }
+  
+  timerstart(){
+    
+  }
+  
+  onenterframe(){} //オーバーライドする
+  
+}
+
