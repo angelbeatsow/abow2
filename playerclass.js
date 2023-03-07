2bea3453710241ba24aa9353ce08f92028790dae
angelbeatsow <117999333+angelbeatsow@users.noreply.github.com>
2023-03-08 02:30:28 +0900

    Create playerclass.js

---
+++ playerclass.js
@@ -0,0 +1,19 @@
+class Player {
+  constructor(){
+    this.name  = '';
+    this.lv    = 1;
+    this.hp    = 100;
+    this.pow   = 10;
+    this.mpow  = 10;
+    this.exp   = 0;
+    this.job   = 0;
+    this.joblv = 1;
+    this.masterjobs = [];
+    this.elements = [];
+    this.maxSetableAtElementLv = [];
+    this.settingElements = [['攻撃']];
+    
+    
+  }
+  
+}
